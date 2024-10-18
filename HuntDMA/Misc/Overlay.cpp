a97ca49b6c68c156aab0dfc930666aa9ebd3ccaf
Денисов Ярослав <dev@brim.su>
2024-10-18 19:39:19 +0300

    chore: change fps text

--- HuntDMA/Misc/Overlay.cpp
+++ HuntDMA/Misc/Overlay.cpp
@@ -9,7 +9,7 @@
 
 void DrawFPS()
 {
-	DrawText(25, 25, L"Overlay FPS: " + std::to_wstring(FrameRate()), "Verdana", Configs.Overlay.FpsFontSize, Configs.Overlay.FpsColour, None);
+	DrawText(25, 25, L"Overlay fps: " + std::to_wstring(FrameRate()), "Verdana", Configs.Overlay.FpsFontSize, Configs.Overlay.FpsColour, None);
 }
 
 void DrawObjectCount()
